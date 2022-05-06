ecb4d6134117f28a0623315a4553096232ddf01d
supercoolfire <u285916669@byui.edu>
2022-05-07 04:04:40 +0800

    water cycle

---
+++ start/start/js/script.js
@@ -0,0 +1,5 @@
+let myDate = new Date();
+let myYear = myDate.getFullYear();
+document.getElementById("theYear").textContent = myYear;
+
+document.getElementById("lu").textContent = "Last Updated: " + document.lastModified;
