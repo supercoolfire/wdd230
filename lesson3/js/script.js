404b8d957b1ab6ea366659c3ada16a59ccab15ce
supercoolfire <u285916669@byui.edu>
2022-05-18 04:17:19 +0800

    udpate

---
+++ lesson3/js/script.js
@@ -0,0 +1,8 @@
+
+// footer
+let myDate = new Date();
+let myYear = myDate.getFullYear();
+document.getElementById("theYear").textContent = myYear;
+
+document.querySelector("#lu").textContent = `Last Modification:  ${document.lastModified}`;
+
