3f425718c5b8e264dd128a33b6367a10510c7833
supercoolfire <u285916669@byui.edu>
2022-05-18 21:41:04 +0800

    upload

---
+++ chamber/js/script.js
@@ -0,0 +1,24 @@
+// hamburger
+function toggleMenu() {
+    console.log("it worked");
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
+
+
+// footer
+let myDate = new Date();
+let myYear = myDate.getFullYear();
+document.getElementById("theYear").textContent = myYear;
+
+document.querySelector("#lu").textContent = `Last Modification:  ${document.lastModified}`;
+
+// custom
+document.querySelector(".myName").textContent = "Jayser Pilapil";
+let theTitle = document.querySelector("h1").textContent;
+let theTagline = document.querySelector("h2").textContent;
+document.querySelector("title").textContent = `${theTitle} ${theTagline}`;
+
