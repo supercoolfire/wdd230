d8c70993440877ea765e2c57cb10802ea94863c2
supercoolfire <u285916669@byui.edu>
2022-05-18 19:57:14 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// hamburger
+function toggleMenu() {
+    console.log("it worked");
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
+
+
+// footer
 let myDate = new Date();
 let myYear = myDate.getFullYear();
 document.getElementById("theYear").textContent = myYear;
