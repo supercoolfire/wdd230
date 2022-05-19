6546f2a762270262b7a79d8d5c3674c86df1fdb9
supercoolfire <u285916669@byui.edu>
2022-05-20 00:30:27 +0800

    udpate

--- lesson3/js/script.js
+++ lesson3/js/script.js
@@ -6,3 +6,8 @@ document.getElementById("theYear").textContent = myYear;
 
 document.querySelector("#lu").textContent = `Last Modification:  ${document.lastModified}`;
 
+// custom
+let myName = document.querySelector(".myName");
+myName.textContent = "Jayser Pilapil";
+let theTitle = document.querySelector("h1").textContent;
+document.querySelector("title").textContent = `${theTitle}: ${myName}`;
