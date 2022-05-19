991696409b177a43539e85dfdc1e0352068fed96
supercoolfire <u285916669@byui.edu>
2022-05-19 12:50:59 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,12 @@
-// hamburger
-function toggleMenu() {
-    console.log("it worked");
-    document.getElementById("primaryNav").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
-}
-
-const x = document.getElementById("hamburgerBtn");
-x.onclick = toggleMenu;
-
 
 // footer
 let myDate = new Date();
 let myYear = myDate.getFullYear();
 document.getElementById("theYear").textContent = myYear;
-
 document.querySelector("#lu").textContent = `Last Updated:  ${document.lastModified}`;
 
+// custom
+let myName = document.querySelector(".myName");
+myName.textContent = "Jayser Pilapil";
+let theTitle = document.querySelector("h1").textContent;
+document.querySelector("title").textContent = `${theTitle}: ${myName}`;
