3ee2be1f4ff57c3701b315908870de4b837239de
supercoolfire <u285916669@byui.edu>
2022-05-20 16:47:20 +0800

    final finaly finsihed

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -1,8 +1,8 @@
 // hamburger
 function toggleMenu() {
-    console.log("it worked");
-    document.getElementById("primaryNav").classList.toggle("open");
-    document.getElementById("hamburgerBtn").classList.toggle("open");
+  console.log("it worked");
+  document.getElementById("primaryNav").classList.toggle("open");
+  document.getElementById("hamburgerBtn").classList.toggle("open");
 }
 
 const x = document.getElementById("hamburgerBtn");
@@ -12,20 +12,33 @@ x.onclick = toggleMenu;
 // select the elements to manipulate (output to)
 const datefield = document.querySelector(".date");
 const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+const fulldate = new Intl.DateTimeFormat("en-US", {
+  dateStyle: "full"
+}).format(now);
 // datefield.innerHTML = `<em>${fulldate}</em>`;
 datefield.innerHTML = fulldate;
 
 // spam
 function toggleSpam() {
-    console.log("it not worked");
-    document.getElementById("spams").classList.toggle("closness");
-  }
-  
-  const y = document.getElementById("spamClose");
-  y.onclick = toggleSpam;
+  console.log("it not worked");
+  document.getElementById("spams").classList.toggle("closness");
+}
+document.getElementById("spamClose").onclick = toggleSpam;
+
+function toggleBtnJoinNow() {
+  console.log("Tired of woking");
+  document.getElementById("spams").classList.toggle("closness");
+  document.getElementById("join").scrollIntoView();
+}
+document.getElementById("btnJoinNow").onclick = toggleBtnJoinNow;
 
 
+function toggleSignIn() {
+  console.log("work again");
+  document.getElementById("spams").classList.toggle("closness");
+  document.getElementById("spams").scrollIntoView();
+}
+document.getElementById("signIn").onclick = toggleSignIn;
 
 // footer
 let myDate = new Date();
@@ -42,4 +55,3 @@ myName.textContent = "Jayser Pilapil";
 let theTitle = document.querySelector("h1").textContent;
 let theTagline = document.querySelector("h2").textContent;
 document.querySelector("title").textContent = `${theTitle} ${theTagline}`;
-
