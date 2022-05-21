5bef560a45930e958b22dff5f7e396aae20fadc9
supercoolfire <u285916669@byui.edu>
2022-05-22 07:49:25 +0800

    enable enter

--- bom/js/bom.js
+++ bom/js/bom.js
@@ -2,26 +2,35 @@ const list = document.querySelector('#unorderedlist');
 const input = document.querySelector('#inputItem');
 const button = document.querySelector('#addBtn');
 
-button.addEventListener('click', () => {
-  const myItem = input.value;
-  if (myItem !== "") {
-    input.value = '';
-
-    const listItem = document.createElement('li');
-    const listText = document.createElement('span');
-    const listBtn = document.createElement('button');
-
-    listItem.appendChild(listText);
-    listText.textContent = myItem;
-    listItem.appendChild(listBtn);
-    listBtn.innerHTML = "\u274C";
-
-    list.appendChild(listItem);
-    console.log(listItem)
-
-    listBtn.addEventListener('click', () => {
-      list.removeChild(listItem);
-    });
+function processEntry() {
+  {
+    const myItem = input.value;
+    if (myItem !== "") {
+      input.value = '';
+  
+      const listItem = document.createElement('li');
+      const listText = document.createElement('span');
+      const listBtn = document.createElement('button');
+  
+      listItem.appendChild(listText);
+      listText.textContent = myItem;
+      listItem.appendChild(listBtn);
+      listBtn.innerHTML = "\u274C";
+  
+      list.appendChild(listItem);
+      console.log(listItem)
+  
+      listBtn.addEventListener('click', () => {
+        list.removeChild(listItem);
+      });
+    }
+    input.focus();
   }
-  input.focus();
+}
+input.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    processEntry();
+  }
 });
+
+button.addEventListener('click', processEntry );
