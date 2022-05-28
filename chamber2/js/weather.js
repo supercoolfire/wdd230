f0be089231bd2c1f4eed12d07b51917065d795f1
supercoolfire <u285916669@byui.edu>
2022-05-28 21:06:16 +0800

    rebuild weather. new code old result

--- chamber2/js/weather.js
+++ chamber2/js/weather.js
@@ -2,10 +2,10 @@
 const app = {
   init: () => {
     document
-      .getElementById('btnGet')
+      .getElementById('card-btnGet')
       .addEventListener('click', app.fetchWeather);
     document
-      .getElementById('btnCurrent')
+      .getElementById('card-btnCurrent')
       .addEventListener('click', app.getLocation);
     // app.fetchWeather();
   },
@@ -53,41 +53,25 @@ const app = {
     let row = document.querySelector('.weather.weatherRow');
     //clear out the old weather and add the new
     // row.innerHTML = '';
-    row.innerHTML = resp.daily
-      .map((day, idx) => {
+    resp.daily.map((day, idx) => {
         // console.log(day)
         if (idx <= 0) { //number of days
           let dt = new Date(day.dt * 1000); //timestamp * 1000
-          let sr = new Date(day.sunrise * 1000).toTimeString();
-          let ss = new Date(day.sunset * 1000).toTimeString();
-          return `<div class="col">
-              <div class="card">
-              <h3 class="card-title">${dt.toDateString()}</h3>
-              <h4 class="timeZone">${resp.timezone}</h4>
-              <picture class="weatherPicture">
-                <img
-                  src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"
-                  class="card-img-top"
-                  alt="${day.weather[0].description}"
-                />
-                <div class="card-title">${day.weather[0].main}<br>
-                "${day.weather[0].description}"</div>
-                </picture>
-                <div class="card-body">
-                  <p class="card-text">High: <span class="weatherInfo">${day.temp.max}&deg;C</span> Low <span class="weatherInfo">${day.temp.min}&deg;C</span></p>
-                  <p class="card-text">High Feels like: <span class="weatherInfo">${day.feels_like.day}&deg;C</span></p>` +
-            // <p class="card-text">Pressure <span class="weatherInfo">${day.pressure}mb</span></p>
-            `<p class="card-text">Humidity: <span class="weatherInfo">${day.humidity}%</span></p>` +
-            // <p class="card-text">UV Index <span class="weatherInfo">${day.uvi}</span></p>
-            // <p class="card-text">Precipitation <span class="weatherInfo">${day.pop * 100}%</span></p>
-            // <p class="card-text">Dewpoint <span class="weatherInfo">${day.dew_point}</span></p>
-            `<p class="card-text">Wind: <span class="weatherInfo">${day.wind_speed}m/s, ${day.wind_deg}&deg;</span></p>` +
-            // <p class="card-text">Sunrise <span class="weatherInfo">${sr}</span></p>
-            // <p class="card-text">Sunset <span class="weatherInfo">${ss}</span></p>
-            `</div>
-              </div>
-            </div>
-          </div>`;
+          document.getElementById("card-date").textContent = dt.toDateString();
+          document.getElementById("card-timeZone").textContent = resp.timezone;
+          document.getElementById("card-image").innerHTML = `<img
+          src="https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png"
+          class="card-img-top"
+          alt="${day.weather[0].description}"
+        />`;
+          document.getElementById("card-temperature").innerHTML = `<span id="card-hotness">${day.temp.max}</span>&deg;C`;
+          document.getElementById("card-prhase").textContent = day.weather[0].main;
+          document.getElementById("card-reprhase").textContent = day.weather[0].description;
+          document.getElementById("card-hiTemp").innerHTML = `${day.temp.max}&deg;C`;
+          document.getElementById("card-lowTemp").innerHTML = `${day.temp.min}&deg;C`;
+          document.getElementById("card-feelTemp").innerHTML = `${day.feels_like.day}&deg;C`;
+          document.getElementById("card-humidity").textContent = `${day.humidity}%`;
+          document.getElementById("card-windSpeed").innerHTML = `<span ="speedy-gonzales">${day.wind_speed}</span>m/s, ${day.wind_deg}&deg;`;
         }
       })
       .join(' ');
