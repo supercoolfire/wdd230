db1cc714f2cdae481438db4af173fd5924572ba6
supercoolfire <u285916669@byui.edu>
2022-05-29 03:38:07 +0800

    Weather fortune  teller

---
+++ weather-summary/js/script.js
@@ -0,0 +1,6 @@
+// footer
+let myDate = new Date();
+let myYear = myDate.getFullYear();
+document.getElementById("theYear").textContent = myYear;
+
+document.querySelector("#lu").textContent = `Last Modification:  ${document.lastModified}`;
