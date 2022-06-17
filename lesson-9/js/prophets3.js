bf0f29c00cbea75d2b3ede41209be60cae0a471f
supercoolfire <u285916669@byui.edu>
2022-06-17 19:45:49 +0800

    wow

--- lesson-9/js/prophets3.js
+++ lesson-9/js/prophets3.js
@@ -165,3 +165,19 @@ window.onload = () => {
   // document.querySelector("#searchBtn").addEventListener("click", pro.findIt);
   document.getElementById("sortBy").addEventListener("change", pro.sortSection);
 }
+
+function setTextAnimation(delay, duration, strokeWidth, timingFunction, strokeColor,repeat) {
+            let paths = document.querySelectorAll("path");
+            let mode=repeat?'infinite':'forwards'
+            for (let i = 0; i < paths.length; i++) {
+                const path = paths[i];
+                const length = path.getTotalLength();
+                path.style["stroke-dashoffset"] = `${length}px`;
+                path.style["stroke-dasharray"] = `${length}px`;
+                path.style["stroke-width"] = `${strokeWidth}px`;
+                path.style["stroke"] = `${strokeColor}`;
+                path.style["animation"] = `${duration}s svg-text-anim ${mode} ${timingFunction}`;
+                path.style["animation-delay"] = `${i * delay}s`;
+            }
+        }
+ setTextAnimation(0.1,5,1,'step-end','#ff004c',true);
